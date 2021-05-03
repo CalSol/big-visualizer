62354b82b678c6d3b4f47694b6e14991b3dfc01c
Richard Lin <elpato25@gmail.com>
2021-05-02 21:13:09 -0700

    main panel

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,45 +2,68 @@ package bigvis
 
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.Insets
+import scalafx.beans.property.StringProperty
 import scalafx.scene.Scene
-import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
-import scalafx.scene.paint.Color._
-import scalafx.scene.paint._
-import scalafx.scene.text.Text
+import scalafx.scene.chart.{LineChart, NumberAxis}
+import scalafx.scene.control.{SplitPane, TreeItem, TreeTableColumn, TreeTableView}
+import scalafx.scene.layout.VBox
 
 
-object ScalaFXHelloWorld extends JFXApp {
+// TODO split into data model
+case class DataItem(name: String) {
+  val nameProp = StringProperty(name)
+  val dataProp = StringProperty("0")
+}
+
+
+object Main extends JFXApp {
+  // See layouts documentation
+  // https://docs.oracle.com/javafx/2/layout/builtin_layouts.htm
+
+  val dataRoot = new TreeItem(DataItem("root")) {
+    expanded = true
+    children = Seq(
+      new TreeItem(DataItem("quack")),
+      new TreeItem(DataItem("lol")),
+      new TreeItem(DataItem("cats")),
+    )
+  }
+
+  val navigationPane = new VBox {
+    // See table view example at
+    // https://github.com/scalafx/ScalaFX-Tutorials/blob/master/slick-table/src/main/scala/org/scalafx/slick_table/ContactsView.scala
+    // and tree view example at
+    // https://github.com/scalafx/scalafx/blob/master/scalafx-demos/src/main/scala/scalafx/controls/treetableview/TreeTableViewWithTwoColumns.scala
+    val tree = new TreeTableView[DataItem](dataRoot) {
+      columns ++= Seq(
+        new TreeTableColumn[DataItem, String] {
+          text = "Name"
+          cellValueFactory = { _.value.value.value.nameProp }
+        },
+        new TreeTableColumn[DataItem, String] {
+          text = "Data"
+          cellValueFactory = { _.value.value.value.dataProp }
+        }
+      )
+    }
+
+    children = Seq(tree)
+  }
+
+  val visualizationPane = new VBox {
+    val timeAxis = new NumberAxis()
+    val dataAxis = new NumberAxis()
+    val lineChart = LineChart(timeAxis, dataAxis)
+    lineChart.title = "TestChart"
+
+    children = Seq(lineChart)
+  }
 
   stage = new PrimaryStage {
-    title = "ScalaFX Hello World"
+    title = "Big Data Visualizer"
     scene = new Scene {
-      fill = Color.rgb(38, 38, 38)
-      content = new HBox {
-        padding = Insets(50, 80, 50, 80)
-        children = Seq(
-          new Text {
-            text = "Scala"
-            style = "-fx-font: normal bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Red, DarkRed))
-          },
-          new Text {
-            text = "FX"
-            style = "-fx-font: italic bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(White, DarkGray)
-            )
-            effect = new DropShadow {
-              color = DarkGray
-              radius = 15
-              spread = 0.25
-            }
-          }
-        )
+      content = new SplitPane {
+        items ++= Seq(navigationPane, visualizationPane)
       }
     }
   }
