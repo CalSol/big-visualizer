c4addbf9311e23d44308f5986a41ab6471e5287f
Richard Lin <elpato25@gmail.com>
2021-05-02 21:19:54 -0700

    example linechart

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,8 +3,9 @@ package bigvis
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.beans.property.StringProperty
+import scalafx.collections.ObservableBuffer
 import scalafx.scene.Scene
-import scalafx.scene.chart.{LineChart, NumberAxis}
+import scalafx.scene.chart.{LineChart, NumberAxis, XYChart}
 import scalafx.scene.control.{SplitPane, TreeItem, TreeTableColumn, TreeTableView}
 import scalafx.scene.layout.VBox
 
@@ -55,6 +56,23 @@ object Main extends JFXApp {
     val dataAxis = new NumberAxis()
     val lineChart = LineChart(timeAxis, dataAxis)
     lineChart.title = "TestChart"
+
+    val data = ObservableBuffer(Seq(
+      (1, 23),
+      (2, 14),
+      (3, 15),
+      (4, 24),
+      (5, 34),
+      (6, 36),
+      (7, 22),
+      (8, 45),
+      (9, 43),
+      (10, 17),
+      (11, 29),
+      (12, 25)
+    ) map {case (x, y) => XYChart.Data[Number, Number](x, y)})
+    val series = XYChart.Series[Number, Number]("test", data)
+    lineChart.getData.add(series)
 
     children = Seq(lineChart)
   }
