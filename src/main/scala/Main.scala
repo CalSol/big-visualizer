fbb7bf397621c2cf301234a8959a0c2c9c58e668
Richard Lin <elpato25@gmail.com>
2021-08-22 16:39:03 -0700

    jank axes synchronization

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -55,26 +55,36 @@ object Main extends JFXApp {
   }
 
   val visualizationPane = new VBox {
-    val timeAxis = new NumberAxis()
-    timeAxis.setAutoRanging(false)
-
     val rawData1 = (0 until 1024).map { i => (i, i + 64 * Math.random()) }
     val data1 = ObservableBuffer(rawData1 map {case (x, y) => XYChart.Data[Number, Number](x, y)})
     val series1 = XYChart.Series[Number, Number]("test1", data1)
+    val time1Axis = new NumberAxis()
+    time1Axis.setAnimated(false)
+    time1Axis.setAutoRanging(false)
     val data1Axis = new NumberAxis()
 //    data1Axis.setAutoRanging(false)
-    val lineChart1 = LineChart(timeAxis, data1Axis)
+    val lineChart1 = LineChart(time1Axis, data1Axis)
     lineChart1.title = "TestChart1"
     lineChart1.getData.add(series1)
+    lineChart1.getYAxis.setTickLabelRotation(270)  // to keep the chart size the same so stacked charts are consistent
+    lineChart1.setLegendVisible(false)
+    lineChart1.getXAxis.setVisible(false)  // only last chart needs visible time axis
+    lineChart1.getXAxis.setTickMarkVisible(false)
+    lineChart1.getXAxis.setTickLabelsVisible(false)
 
     val rawData2 = (0 until 1024).map { i => (i, -i - 64 * Math.random()) }
     val data2 = ObservableBuffer(rawData2 map {case (x, y) => XYChart.Data[Number, Number](x, y)})
     val series2 = XYChart.Series[Number, Number]("test2", data2)
+    val time2Axis = new NumberAxis()
+    time2Axis.setAnimated(false)
+    time2Axis.setAutoRanging(false)
     val data2Axis = new NumberAxis()
 //    data2Axis.setAutoRanging(false)
-    val lineChart2 = LineChart(timeAxis, data2Axis)
+    val lineChart2 = LineChart(time2Axis, data2Axis)
     lineChart2.title = "TestChart2"
     lineChart2.getData.add(series2)
+    lineChart2.getYAxis.setTickLabelRotation(270)  // to keep the chart size the same so stacked charts are consistent
+    lineChart2.setLegendVisible(false)
 
     children = Seq(lineChart1, lineChart2)
     setVgrow(lineChart1, Priority.Always)
@@ -83,22 +93,20 @@ object Main extends JFXApp {
     val SCALE_DELTA = 1.1
     lineChart2.setOnScroll(new EventHandler[ScrollEvent] {
       override def handle(event: ScrollEvent): Unit = {
-        println(s"scroll ${event.getDeltaX} ${event.getDeltaY}")
         event.consume()
         if (event.getDeltaY == 0) {
           return
         }
         val scaleFactor = if (event.getDeltaY > 0) SCALE_DELTA else 1 / SCALE_DELTA
-        timeAxis.setLowerBound(timeAxis.getLowerBound + event.getDeltaY)
-        timeAxis.setUpperBound(timeAxis.getUpperBound + event.getDeltaY)
-        println(s"${timeAxis.getLowerBound}, ${timeAxis.getUpperBound}")
+        time1Axis.setLowerBound(time1Axis.getLowerBound + event.getDeltaY)
+        time1Axis.setUpperBound(time1Axis.getUpperBound + event.getDeltaY)
+        time2Axis.setLowerBound(time2Axis.getLowerBound + event.getDeltaY)
+        time2Axis.setUpperBound(time2Axis.getUpperBound + event.getDeltaY)
       }
     })
 
     lineChart2.setOnMousePressed(new EventHandler[MouseEvent]() {
       override def handle(event: MouseEvent): Unit = {
-        println(s"press ${event.getClickCount}")
-
         if (event.getClickCount == 2) {
           lineChart2.setScaleX(1.0)
           lineChart2.setScaleY(1.0)
