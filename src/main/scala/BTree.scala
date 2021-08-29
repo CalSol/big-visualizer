a5a70bc7c599485150dcde8926fba3108b807435
Richard Lin <elpato25@gmail.com>
2021-08-29 15:18:23 -0700

    basic operating

--- src/main/scala/BTree.scala
+++ src/main/scala/BTree.scala
@@ -7,11 +7,11 @@ object BTree {
 }
 
 abstract class BTreeAggregator[NodeType, LeafType] {
-  // Compute intermediate node data, for a node consisting of only leaves
-  // Must be able to handle the empty case
+  // Compute intermediate node data, for a node consisting of only leaves.
+  // data guaranteed non-empty
   def fromLeaves(data: Seq[(BTree.TimestampType, LeafType)]): NodeType
   // Compute intermediate node data, for a node consisting of only sub-nodes
-  // Must be able to handle the empty case
+  // data guaranteed non-empty
   def fromNodes(data: Seq[((BTree.TimestampType, BTree.TimestampType), NodeType)]): NodeType
 }
 
@@ -23,17 +23,48 @@ abstract class BTreeAggregator[NodeType, LeafType] {
  */
 class BTree[NodeType, LeafType](val aggregator: BTreeAggregator[NodeType, LeafType],
                                 val nodeSize: Int) {
+  protected var root: BTreeNode[NodeType, LeafType] = new BTreeLeafNode(this)
+
   // Adds the data (as points of (timestamp, data), Data must be ordered, but only within itself
   // (it can overlap with existing points in the tree)
   // Data must not be empty.
-  def appendAll(data: IterableOnce[(BTree.TimestampType, LeafType)]) = ???
+  def appendAll(data: IterableOnce[(BTree.TimestampType, LeafType)]): Unit = {
+    var remainingData = data.toSeq
+    while (remainingData.nonEmpty) {
+      remainingData = root.appendAll(remainingData)
+      if (remainingData.nonEmpty) {  // split node and insert new root
+        val leftNode = root
+        val rightNode = root.split()
+
+        val newRoot = new BTreeIntermediateNode(this)
+        newRoot.appendNodes(Seq(leftNode, rightNode))
+        root = newRoot
+      }
+    }
+  }
 
   // Returns all the leaf points as a seq
-  def toSeq: Seq[(BTree.TimestampType, LeafType)] = ???
+  def toSeq: Seq[(BTree.TimestampType, LeafType)] = {
+    toLeafChain.flatMap(_.leaves)
+  }
+
+  protected def toLeafChain: Seq[BTreeLeafNode[NodeType, LeafType]] = {
+    def traverse(node: BTreeNode[NodeType, LeafType]): Seq[BTreeLeafNode[NodeType, LeafType]] = node match {
+      case node: BTreeIntermediateNode[NodeType, LeafType] => node.nodes.toSeq flatMap(traverse)
+      case node: BTreeLeafNode[NodeType, LeafType] => Seq(node)
+    }
+    traverse(root)
+  }
 
   // Returns the maximum depth of the tree, excluding leaf entries
   // Expensive! This traverses the entire tree!
-  def maxDepth: Int = ???
+  def maxDepth: Int = {
+    def traverse(node: BTreeNode[NodeType, LeafType]): Int = node match {
+      case node: BTreeIntermediateNode[NodeType, LeafType] => node.nodes.toSeq.map(traverse).max + 1
+      case node: BTreeLeafNode[NodeType, LeafType] => 1
+    }
+    traverse(root)
+  }
 }
 
 // Internal data structure, base class for a tree node
@@ -57,23 +88,25 @@ abstract class BTreeNode[NodeType, LeafType](root: BTree[NodeType, LeafType]) {
 }
 
 // B-tree node that contains an array of leaves
-class BTreeLeafNode[NodeType, LeafType](root: BTree[NodeType, LeafType],
-                                        parent: Option[BTreeNode[NodeType, LeafType]])
+class BTreeLeafNode[NodeType, LeafType](root: BTree[NodeType, LeafType])
     extends BTreeNode[NodeType, LeafType](root) {
-  protected val leaves = mutable.ArrayBuffer[(BTree.TimestampType, LeafType)]()
-  var nodeData: NodeType = root.aggregator.fromLeaves(Seq())  // intermediate node data
+  protected[bigvis] var leaves = mutable.ArrayBuffer[(BTree.TimestampType, LeafType)]()
+  protected var internalNodeData: Option[NodeType] = None  // intermediate node data
+  override def nodeData: NodeType = internalNodeData.get
 
   // Initialized with invalid values when empty
-  var minTime: BTree.TimestampType = Long.MaxValue
-  var maxTime: BTree.TimestampType = Long.MinValue
+  protected var internalMinTime: BTree.TimestampType = Long.MaxValue
+  override def minTime = internalMinTime
+  protected var internalMaxTime: BTree.TimestampType = Long.MinValue
+  override def maxTime = internalMaxTime
 
   def appendAll(data: Seq[(BTree.TimestampType, LeafType)]): Seq[(BTree.TimestampType, LeafType)] = {
     // Insert data until full
     // When full, split the node in the parent, recursively as needed, and delegate continued data adding
     require(data.nonEmpty)  // empty appends handled at BTree level
-    require(data.head._1 >= maxTime, "TODO: support insertions not at end")  // I'm a lazy duck
+    require(data.head._1 >= internalMaxTime, "TODO: support insertions not at end")  // I'm a lazy duck
 
-    var currTime = maxTime
+    var currTime = internalMaxTime
     var remainingData = data
     while (leaves.length < root.nodeSize && remainingData.nonEmpty) {
       val head = remainingData.head
@@ -84,51 +117,109 @@ class BTreeLeafNode[NodeType, LeafType](root: BTree[NodeType, LeafType],
     }
 
     // update this node
-    minTime = leaves.head._1
-    maxTime = leaves.last._1
-    nodeData = root.aggregator.fromLeaves(leaves.toSeq)
+    internalMinTime = leaves.head._1
+    internalMaxTime = leaves.last._1
+    internalNodeData = Some(root.aggregator.fromLeaves(leaves.toSeq))
 
     remainingData  // return any remaining data - indicates this node needs to be split
   }
 
-  def split(): BTreeLeafNode[NodeType, LeafType] {
-    
+  def split(): BTreeLeafNode[NodeType, LeafType] = {
+    require(leaves.length == root.nodeSize, "can't split before at max size")
+    val (leftSplit, rightSplit) = leaves.splitAt(root.nodeSize / 2)
+
+    // create right node
+    val rightNode = new BTreeLeafNode(root)
+    val rightLeftover = rightNode.appendAll(rightSplit.toSeq)
+    require(rightLeftover.isEmpty, "split should not overflow")
+
+    // update this node
+    leaves = leftSplit
+    internalMinTime = leaves.head._1
+    internalMaxTime = leaves.last._1
+    internalNodeData = Some(root.aggregator.fromLeaves(leaves.toSeq))
+
+    rightNode
   }
 
   def validate(): Boolean = {
     // TODO: validation fails on empty leaves
-    minTime == leaves.head._1 && maxTime == leaves.last._1 && leaves.size <= root.nodeSize &&
-        nodeData == root.aggregator.fromLeaves(leaves.toSeq)
+    internalMinTime == leaves.head._1 && internalMaxTime == leaves.last._1 && leaves.size <= root.nodeSize &&
+        internalNodeData == root.aggregator.fromLeaves(leaves.toSeq)
   }
 }
 
 // B-tree node that contains an array of other nodes
-class BTreeIntermediateNode[NodeType, LeafType](root: BTree[NodeType, LeafType],
-                                                parent: Option[BTreeNode[NodeType, LeafType]])
+class BTreeIntermediateNode[NodeType, LeafType](root: BTree[NodeType, LeafType])
     extends BTreeNode[NodeType, LeafType](root) {
-  protected val nodes = mutable.ArrayBuffer[BTreeNode[NodeType, LeafType]]()
-  var nodeData: NodeType = root.aggregator.fromNodes(Seq())  // intermediate node data
+  protected[bigvis] var nodes = mutable.ArrayBuffer[BTreeNode[NodeType, LeafType]]()
+  protected var internalNodeData: Option[NodeType] = None  // intermediate node data
+  override def nodeData: NodeType = internalNodeData.get
 
   // Initialized with invalid values when empty
-  var minTime: BTree.TimestampType = Long.MaxValue
-  var maxTime: BTree.TimestampType = Long.MinValue
+  protected var internalMinTime: BTree.TimestampType = Long.MaxValue
+  override def minTime = internalMinTime
+  protected var intermalMaxTime: BTree.TimestampType = Long.MinValue
+  override def maxTime = intermalMaxTime
 
   def appendAll(data: Seq[(BTree.TimestampType, LeafType)]): Seq[(BTree.TimestampType, LeafType)] = {
     // Insert data until full
     // When full, split the node in the parent, recursively as needed
-    require(data.nonEmpty)  // empty appends handled at BTree level
-    require(data.head._1 >= maxTime, "TODO: support insertions not at end")  // I'm a lazy duck
+    if (nodes.isEmpty) {
+      nodes.append(new BTreeLeafNode(root))
+    }
+    require(data.head._1 >= intermalMaxTime, "TODO: support insertions not at end")  // I'm a lazy duck
 
     var remainingData = nodes.last.appendAll(data)
-    if (remainingData.nonEmpty) {  // must split node
-      if (nodes.length < root.nodeSize) {  // can split node
+    while (remainingData.nonEmpty && nodes.length < root.nodeSize) {  // while this node can still do useful work
+      // Split the node we just inserted into
+      val newLast = nodes.last.split()
+      nodes.append(newLast)
 
-      } else {  // can't split node, punt
-        remainingData
-      }
-    } else {
-      Seq()  // done
+      remainingData = newLast.appendAll(data)
     }
+
+    internalMinTime = nodes.head.minTime
+    intermalMaxTime = nodes.last.maxTime
+    internalNodeData = Some(root.aggregator.fromNodes(nodes.toSeq.map { node =>
+      ((node.minTime, node.maxTime), node.nodeData)
+    }))
+
+    remainingData.toSeq
+  }
+
+
+  // Internal API to directly add nodes, instead of appending data points
+  protected[bigvis] def appendNodes(addNodes: Seq[BTreeNode[NodeType, LeafType]]): Unit = {
+    require(nodes.isEmpty, "can't appendNodes with existing nodes")
+    require(addNodes.length <= root.nodeSize, "appendNodes must not overflow")
+    nodes.appendAll(addNodes)
+
+    internalMinTime = nodes.head.minTime
+    intermalMaxTime = nodes.last.maxTime
+    internalNodeData = Some(root.aggregator.fromNodes(nodes.toSeq.map { node =>
+      ((node.minTime, node.maxTime), node.nodeData)
+    }))
+
+  }
+
+  def split(): BTreeIntermediateNode[NodeType, LeafType] = {
+    require(nodes.length == root.nodeSize, "can't split before at max size")
+    val (leftSplit, rightSplit) = nodes.splitAt(root.nodeSize / 2)
+
+    // create right node
+    val rightNode = new BTreeIntermediateNode(root)
+    rightNode.appendNodes(rightSplit.toSeq)
+
+    // update this node
+    nodes = leftSplit
+    internalMinTime = nodes.head.minTime
+    intermalMaxTime = nodes.last.maxTime
+    internalNodeData = Some(root.aggregator.fromNodes(nodes.toSeq.map { node =>
+      ((node.minTime, node.maxTime), node.nodeData)
+    }))
+
+    rightNode
   }
 
   def validate(): Boolean = {
@@ -139,8 +230,8 @@ class BTreeIntermediateNode[NodeType, LeafType](root: BTree[NodeType, LeafType],
     val expectedData = root.aggregator.fromNodes(nodes.map { node =>
       ((node.minTime, node.maxTime), node.nodeData)
     }.toSeq)
-    nodes.nonEmpty && minTime == nodes.head.minTime && maxTime == nodes.last.maxTime &&
+    nodes.nonEmpty && internalMinTime == nodes.head.minTime && intermalMaxTime == nodes.last.maxTime &&
         nodesValidated && timesOrdered &&
-        nodes.size <= root.nodeSize && nodeData == expectedData
+        nodes.size <= root.nodeSize && internalNodeData == expectedData
   }
 }
