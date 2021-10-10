4bc314ab726d22081a2226386deeb971fdc27cb8
Richard Lin <elpato25@gmail.com>
2021-10-10 03:09:31 -0700

    wip

--- src/main/scala/CursorCanvas.scala
+++ src/main/scala/CursorCanvas.scala
@@ -2,13 +2,69 @@ package bigvis
 
 import btree.{BTreeAggregate, BTreeData, BTreeLeaf, FloatAggregator}
 
+import scala.collection.mutable
+
 
 object CursorCanvas {
+  /** A data object representing groups of labels, with the bounds of the data they represent.
+   *
+   * @param dataMin, dataMax bounds considering only the data, not accounting for label size
+   * @param count number of labels in this group
+   * @param viewMin, viewMax bounds considering label size
+   */
+  case class LabelGroup(dataMin: Double, dataMax: Double, count: Int,
+                        viewMin: Double, viewMax: Double) {
+    def mergeWith(other: LabelGroup, limitMin: Double, limitMax: Double, spread: Double): LabelGroup = {
+      val newDataMinPos = math.min(dataMin, other.dataMin)
+      val newDataMaxPos = math.max(dataMax, other.dataMax)
+      val newCount = count + other.count
+      val halfSize = newCount * spread / 2
+      val viewSize = limitMax - limitMin
+      val newCenter = (newDataMinPos + newDataMaxPos) / 2
+      val (newViewMinPos, newViewMaxPos) = if (halfSize * 2 > viewSize) {
+        // area larger than limits - center everything because it's the only way
+        val limitCenter = (limitMin + limitMax) / 2
+        (limitCenter - halfSize, limitCenter + halfSize)
+      } else if (newCenter - halfSize < limitMin) {  // otherwise, we only need to clip lower or upper (but not both)
+        (limitMin, limitMin + halfSize * 2)
+      } else if (newCenter + halfSize > limitMax) {
+        (limitMax - halfSize * 2, limitMax)
+      } else {
+        (newCenter - halfSize, newCenter + halfSize)
+      }
+
+      LabelGroup(newDataMinPos, newDataMaxPos, count + newCount, newViewMinPos, newViewMaxPos)
+    }
+
+    def overlapsWith(other: LabelGroup): Boolean = {
+
+    }
+  }
+
   /** Given a list of originalPositions, spreads them out such that they are at least minSpread apart
    * while bounded by min and max.
+   *
+   * Algorithm: merge positions that are less than minSpread apart, placing them minSpread apart but centered
+   * at the midpoint of the lowest and highest original positions in the merge group.
+   * Repeat until there are no more positions to be merged (fixed-point algorithm).
    */
   def spreadPositions(originalPositions: Seq[Double], minSpread: Double, min: Double, max: Double): Seq[Double] = {
-    originalPositions
+    var positions = originalPositions.map { pos =>
+      LabelGroup(pos, pos, 1, pos, pos)
+    }
+    // Repeatedly merge
+    if (positions.nonEmpty) {
+      val newPositions = mutable.ListBuffer[LabelGroup]()
+      var done = false
+      while (!done) {
+        var last = positions.head
+        positions.tail.foreach { next =>
+
+        }
+      }
+    }
+    // Transform to individual coordinates
+    positions
   }
 }
 
