0b0985caca8f1ea935a8a2e642e4c590d93fafcb
Richard Lin <elpato25@gmail.com>
2021-11-12 01:43:02 -0800

    csv parser code, partial

--- src/main/scala/CsvLoader.scala
+++ src/main/scala/CsvLoader.scala
@@ -9,36 +9,71 @@ import scala.jdk.CollectionConverters.{IteratorHasAsScala, ListHasAsScala}
 import scala.util.Try
 
 
+trait DataBuilder {
+  val name: String
+}
+
+
 trait Parser {
   def parseCell(time: Long, value: String): Unit
+  def getBuilder: DataBuilder
 }
 
 
-class StringParser extends Parser {
+class DummyParser(val name: String) extends Parser {
+  def parseCell(time: Long, value: String): Unit = {}
+
+  override def getBuilder: DataBuilder = new DataBuilder {
+    override val name = DummyParser.this.name
+  }
+}
+
+
+class StringParser(val name: String) extends Parser with DataBuilder {
+  override def toString: String = s"${getClass.getName}($name)"
+
   protected val dataBuilder = mutable.ArrayBuffer[(Long, String)]()
 
-  def parseCell(time: Long, value: String): Unit = {
+  override def parseCell(time: Long, value: String): Unit = {
+    if (value.isEmpty) {
+      return
+    }
+
     dataBuilder.append((time, value))
   }
+
+  override def getBuilder: DataBuilder = this
 }
 
 
-class DoubleParser extends Parser {
+class DoubleParser(val name: String) extends Parser with DataBuilder {
+  override def toString: String = s"${getClass.getName}($name)"
+
   protected val dataBuilder = mutable.ArrayBuffer[(Long, Double)]()
 
-  def parseCell(time: Long, value: String): Unit = {
+  override def parseCell(time: Long, value: String): Unit = {
+    if (value.isEmpty) {
+      return
+    }
     dataBuilder.append((time, value.toDouble))
   }
+
+  override def getBuilder: DataBuilder = this
 }
 
 
-class DoubleArrayBuilder {
+class DoubleArrayBuilder(val name: String) extends DataBuilder {
   protected val dataBuilder = mutable.ArrayBuffer[(Long, Map[Int, Double])]()
   protected var assemblyTime: Option[Long] = None
   protected val assembly = mutable.HashMap[Int, Double]()
 
   class CellParser(index: Int) extends Parser {
+    override def toString: String = s"${getClass.getName}($name, $index)"
+
     def parseCell(time: Long, value: String): Unit = {
+      if (value.isEmpty) {
+        return
+      }
       if (assemblyTime != Some(time)) {
         if (assembly.nonEmpty) {
           dataBuilder.append((assemblyTime.get, assembly.toMap))
@@ -48,6 +83,8 @@ class DoubleArrayBuilder {
       }
       assembly.put(index, value.toDouble)
     }
+
+    override def getBuilder: DataBuilder = DoubleArrayBuilder.this
   }
 }
 
@@ -73,11 +110,46 @@ object CsvLoader {
         case Seq() => None
       }
     }
+    val dataTypesMap = (headers zip dataTypes).toMap
 
-    val potentialArrays = headers.groupBy(str => str.reverse.dropWhile(_.isDigit).reverse)
+    val headerArrays = headers.groupBy(str => str.reverse.dropWhile(_.isDigit).reverse)
         .filter(_._2.length >= ARRAY_MIN_LEN)
 
+    val headerDoubleArrays = headerArrays.filter { case (arrayPrefix, arrayElts) =>
+      arrayElts.forall(dataTypesMap.getOrElse(_, None) == Some(classOf[Double]))
+    }
+    val doubleArraysMap = headerDoubleArrays.flatMap { case (arrayPrefix, arrayElts) =>
+      val builder = new DoubleArrayBuilder(arrayPrefix)
+      arrayElts.map { arrayElt =>
+        val index = arrayElt.reverse.takeWhile(_.isDigit).reverse.toInt
+        arrayElt -> new builder.CellParser(index)
+      }
+    }
 
-    println(potentialArrays)
+    val parsers = (headers zip dataTypes) map {
+      case (header, dataType) if doubleArraysMap.contains(header) =>
+        doubleArraysMap(header)
+      case (header, Some(dataType)) if dataType == classOf[Double] =>
+        new DoubleParser(header)
+      case (header, Some(dataType)) if dataType == classOf[String] =>
+        new StringParser(header)
+      case (header, None) =>
+        new DummyParser(header)
+    }
+
+    var count: Long = 0
+    val loadTime = timeExec {
+      (firstRows ++ rowIter).foreach { rawRow =>
+        val row = rawRow.getFields.asScala
+        val time = (row.head.toDouble * 1000).toLong
+        (row.tail zip parsers).foreach { case (cell, parser) =>
+          parser.parseCell(time, cell)
+        }
+        
+        count += 1
+      }
+    }
+
+    println(f"loaded $count rows in $loadTime s")
   }
 }
