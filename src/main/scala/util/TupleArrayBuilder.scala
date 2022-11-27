d582b68188bc68496078a0876ffffcc292d64328
Richard Lin <richard.lin@berkeley.edu>
2022-11-27 14:13:07 -0800

    wip fast slicing

--- src/main/scala/util/TupleArrayBuilder.scala
+++ src/main/scala/util/TupleArrayBuilder.scala
@@ -45,12 +45,18 @@ class TupleArrayBuilder[@specialized(Long) T1, @specialized T2](implicit t1: Cla
 /** A data structure that looks like Array[(T1, T2)], but using two Array[T] inside
  * which allows for space- and time- efficient unboxed types with T is primitive.
  *
+ * Supports fast slicing with an optional starting and ending index.
+ *
  * Probably trades off some memory locality, but if the types were boxed and on the heap
  * then there might not have been much memory locality to begin with.
  */
-class TupleArray[@specialized(Long) T1, @specialized T2](val array1: Array[T1], val array2: Array[T2])
+class TupleArray[@specialized(Long) T1, @specialized T2](val array1: Array[T1], val array2: Array[T2],
+                                                         val startIndex: Int = 0, endIndex: Int = -1)
                                                         (implicit t1: ClassTag[T1], t2: ClassTag[T2]) {
   require(array1.length == array2.length)
+  require(startIndex >= 0 && startIndex <= array1.length)  // if equal length, means empty array
+  require((endIndex >= 0 || endIndex == -1) && (endIndex <= array1.length))
+  val actualEndIndex: Int = if (endIndex == -1) array1.length else endIndex
 
   def this()(implicit t1: ClassTag[T1], t2: ClassTag[T2]) = {  // empty array constructor
     this(Array[T1](), Array[T2]())
@@ -58,41 +64,52 @@ class TupleArray[@specialized(Long) T1, @specialized T2](val array1: Array[T1], 
 
   // Converts this to an Array[(T1, T2)], which loses any unboxing benefits
   def toArraySlow: Array[(T1, T2)] = {
-    array1 zip array2
+    array1.slice(startIndex, actualEndIndex) zip array2.slice(startIndex, actualEndIndex)
   }
 
   // Creates a builder initialized with the data in this array
   def toBuilder: TupleArrayBuilder[T1, T2] = {
     val builder = new TupleArrayBuilder[T1, T2]()
-    builder.addAll(array1, array2)
+    var i: Int = startIndex
+    while (i < actualEndIndex) {
+      builder.addOne(array1(i), array2(i))
+      i = i + 1
+    }
     builder
   }
 
-  def length: Int = array1.length
-  def isEmpty: Boolean = array1.isEmpty
-  def nonEmpty: Boolean = array1.nonEmpty
+  def length: Int = actualEndIndex - startIndex
+  def isEmpty: Boolean = length == 0
+  def nonEmpty: Boolean = length > 0
 
   // Same as Array[(T1, T2)].head._1 / ._2 but without needing a tuple wrapper
-  def head_1: T1 = array1.head
-  def head_2: T2 = array2.head
+  def head_1: T1 = array1(startIndex)
+  def head_2: T2 = array2(startIndex)
   // Same as Array[(T1, T2)].last._1 / ._2 but without needing a tuple wrapper
-  def last_1: T1 = array1.last
-  def last_2: T2 = array2.last
+  def last_1: T1 = array1(actualEndIndex - 1)
+  def last_2: T2 = array2(actualEndIndex - 1)
 
   def tail: TupleArray[T1, T2] = {
-    new TupleArray(array1.tail, array2.tail)
+    new TupleArray(array1, array2, startIndex + 1, endIndex)
   }
 
+  // Concatenates two arrays. Slow operation.
   def ++(that: TupleArray[T1, T2]): TupleArray[T1, T2] = {
-    new TupleArray(array1 ++ that.array1, array2 ++ that.array2)
+    val builder = toBuilder
+    var thatI: Int = that.startIndex
+    while (thatI < that.actualEndIndex) {
+      builder.addOne(that.array1(thatI), that.array2(thatI))
+      thatI = thatI + 1
+    }
+    builder.result()
   }
 
   def filter(fn: (T1, T2) => Boolean): TupleArray[T1, T2] = {
     // Imperative operations are much faster than something like zip/zipped:
     // https://stackoverflow.com/questions/59598239/why-is-zipped-faster-than-zip-in-scala
     val outputBuilder = new TupleArrayBuilder[T1, T2]()
-    var i: Int = 0
-    while (i < array1.length) {
+    var i: Int = startIndex
+    while (i < actualEndIndex) {
       if (fn(array1(i), array2(i))) {
         outputBuilder.addOne(array1(i), array2(i))
       }
@@ -103,8 +120,8 @@ class TupleArray[@specialized(Long) T1, @specialized T2](val array1: Array[T1], 
 
   def map[V1](fn: (T1, T2) => V1)(implicit v1: ClassTag[V1]): Array[V1] = {
     val outputBuilder = Array.newBuilder[V1]
-    var i: Int = 0
-    while (i < array1.length) {
+    var i: Int = startIndex
+    while (i < actualEndIndex) {
       outputBuilder.addOne(fn(array1(i), array2(i)))
       i = i + 1
     }
@@ -112,8 +129,7 @@ class TupleArray[@specialized(Long) T1, @specialized T2](val array1: Array[T1], 
   }
 
   def splitAt(index: Int): (TupleArray[T1, T2], TupleArray[T1, T2]) = {
-    val (array1Left, array1Right) = array1.splitAt(index)
-    val (array2Left, array2Right) = array2.splitAt(index)
-    (new TupleArray(array1Left, array2Left), new TupleArray(array1Right, array2Right))
+    (new TupleArray(array1, array2, startIndex, startIndex + index),
+      new TupleArray(array1, array2, startIndex + index, endIndex))
   }
 }
--- src/test/scala/util/TupleArrayTest.scala
+++ src/test/scala/util/TupleArrayTest.scala
@@ -45,10 +45,18 @@ class TupleArrayTest extends AnyFlatSpec with Matchers {
     val tail = builder.result().tail
     tail.length should equal(2)
     tail.toArraySlow should equal(Seq((2, 3), (4, 5)))
+    tail.head_1 should equal(2)
+    tail.head_2 should equal(3)
+    tail.last_1 should equal(4)
+    tail.last_2 should equal(5)
 
     val tail2 = tail.tail
     tail2.length should equal(1)
     tail2.toArraySlow should equal(Seq((4, 5)))
+    tail2.head_1 should equal(4)
+    tail2.head_2 should equal(5)
+    tail2.last_1 should equal(4)
+    tail2.last_2 should equal(5)
   }
 
   it should "splitAt" in {
@@ -58,15 +66,41 @@ class TupleArrayTest extends AnyFlatSpec with Matchers {
 
     val (first, second) = result.splitAt(1)
     first.toArraySlow should equal(Seq((0, 1)))
+    first.length should equal(1)
+    first.head_1 should equal(0)
+    first.head_2 should equal(1)
+    first.last_1 should equal(0)
+    first.last_2 should equal(1)
     second.toArraySlow should equal(Seq((2, 3), (4, 5)))
+    second.length should equal(2)
+    second.head_1 should equal(2)
+    second.head_2 should equal(3)
+    second.last_1 should equal(4)
+    second.last_2 should equal(5)
 
     val (first0, second0) = result.splitAt(0)
     first0.toArraySlow should equal(Seq())
+    first0.length should equal(0)
+    first0.isEmpty should equal(true)
     second0.toArraySlow should equal(Seq((0, 1), (2, 3), (4, 5)))
+    second0.length should equal(3)
+    second0.isEmpty should equal(false)
+    second0.head_1 should equal(0)
+    second0.head_2 should equal(1)
+    second0.last_1 should equal(4)
+    second0.last_2 should equal(5)
 
     val (first3, second3) = result.splitAt(3)
     first3.toArraySlow should equal(Seq((0, 1), (2, 3), (4, 5)))
+    first3.length should equal(3)
+    first3.isEmpty should equal(false)
+    first3.head_1 should equal(0)
+    first3.head_2 should equal(1)
+    first3.last_1 should equal(4)
+    first3.last_2 should equal(5)
     second3.toArraySlow should equal(Seq())
+    second3.length should equal(0)
+    second3.isEmpty should equal(true)
   }
 
   it should "map" in {
